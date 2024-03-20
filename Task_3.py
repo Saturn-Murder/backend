31108b1c3127957ea0671636265681147eb23a91
Saturn-Murder <Kirillica2008@outlook.com>
2024-03-20 21:19:34 +0300

    Программа не закончена

--- Task_3.py
+++ Task_3.py
@@ -2,15 +2,35 @@ import sqlite3
 
 
 def login():
+    log = input("Введите логин: ")
+    return log
+
+
+def password():
     while True:
-        log = int(input("Введите почту для регистрации: "))
-        if str(log).find("@") == 0:
-            print("Почта должна содержать @")
+        favor = input("Введите пароль: ")
+        if len(favor) < 6:
+            print("Пароль слишком маленький, хотя бы 6 символов")
         else:
             break
-    return log
+    return favor
 
 
 con = sqlite3.connect("localhost.db")
 cursor = con.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS reg
+                (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                login TEXT NOT NULL UNIQUE,
+                password TEXT NOT NULL)
+            """)
+
+users = [login(), password()]
+for i in users:
+    try:
+        cursor.execute("INSERT INTO reg VALUES(?, ?, ?)", (None, users[0], users[1]))
+    except sqlite3.IntegrityError:
+        print("Такой аккаунт уже существует!")
+
 con.commit()
+
+
