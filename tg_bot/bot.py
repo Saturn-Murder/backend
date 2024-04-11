9d7fb1ddea2ee265a5bbbc83a9e70f9ad798f111
Saturn-Murder <150595347+Saturn-Murder@users.noreply.github.com>
2024-04-11 11:44:22 +0300

    Взгляни.py

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -7,19 +7,35 @@ from telebot import types
 bot = telebot.TeleBot('7079792810:AAE7N2lW5wRPOhLOWPE40sK72pUO2Rz3-d8')
 con = sqlite3.connect('bot_database.db')
 cursor = con.cursor()
-
+status = ''
+value = ''
+tg_id = None
+tg_name = None
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS users
                (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                user_id INTEGER UNIQUE,
                name TEXT NOT NULL,
-               status varchar(50))
+               mark INTEGER NOT NULL)
             """)
+
+con.commit()
+cursor.close()
+con.close()
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    global tg_id, tg_name
+    tg_id = message.from_user.id
+    tg_name = message.from_user.username
+    if tg_name == 'Dovi_t':
+        status = 'admin'
+    bot.send_message(message.chat.id, "Hello")
     markup = types.InlineKeyboardMarkup(row_width=3)
+    markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    main_item = types.KeyboardButton("Ебашь!")
+    markup2.add(main_item)
     item1 = types.InlineKeyboardButton("0", callback_data="0")
     item2 = types.InlineKeyboardButton("1", callback_data="1")
     item3 = types.InlineKeyboardButton("2", callback_data="2")
@@ -27,20 +43,51 @@ def start(message):
     item5 = types.InlineKeyboardButton("4", callback_data="4")
     item6 = types.InlineKeyboardButton("5", callback_data="5")
     markup.add(item1, item2, item3, item4, item5, item6)
-    con = sqlite3.connect('bot_database.db')
-    cursor = con.cursor()
-    cursor.execute("INSERT INTO users (user_id, name, status) VALUES (?, ?, ?)", (message.from_user.id, message.from_user.username, 'user'))
-    con.commit()
-    cursor.execute("SELECT status FROM users WHERE user_id = (?)", (message.from_user.id,))
-    status = cursor.fetchone()
-    con.commit()
-    cursor.close()
-    con.close()
-    if status == "user":
-        bot.send_message(message.chat.id, f'Привет {message.from_user.username}. Пожалуйста, оцените качество обслуживания:', reply_markup=markup)
+    if status == 'user':
+        bot.send_message(message.chat.id, "Оцените качество обслуживания:", reply_markup=markup)
     else:
-        return
+        bot.send_message(message.chat.id, "Здравствуйте! Хотите посмотреть статистику?", reply_markup=markup2)
+
+
+if status == "admin":
+    @bot.message_handler(content_types=['text'])
+    def per(message):
+        if message.text == "Ебашь!":
+            con = sqlite3.connect("bot_database.db")
+            cursor = con.cursor()
+            cursor.execute("SELECT (name, mark) FROM users")
+            name, total = cursor.fetchall()
+            bot.send_message(message.chat.id, f'{name}, {total}')
+            con.commit()
+            cursor.close()
+            con.close()
+
+
+if status == 'user':
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback(call):
+        global value, tg_id, tg_name
+        if call.data == "0":
+            value = call.data
+        elif call.data == "1":
+            value = call.data
+        elif call.data == "2":
+            value = call.data
+        elif call.data == "3":
+            value = call.data
+        elif call.data == "4":
+            value = call.data
+        elif call.data == "5":
+            value = call.data
+        con = sqlite3.connect('bot_database.db')
+        cursor = con.cursor()
+        cursor.execute("INSERT INTO users (user_id, name, mark) VALUES (?, ?, ?)", (tg_id, tg_name, value))
+        con.commit()
+        cursor.close()
+        con.close()
+        bot.send_message(call.message.chat.id, "Ваша оценка сохранена!")
     
+
 
 bot.polling(none_stop=True)
 
