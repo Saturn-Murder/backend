2be8b2dc7233c1930942611d26b330de1013adfd
Dovi <michael.tabashnikov@gmail.com>
2024-04-16 17:50:30 +0300

    New bot

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -1,94 +1,30 @@
-import telebot
-import sqlite3
-from telebot import types
+import asyncio
+
+from aiogram import Bot, Dispatcher, types, filters
+
+TOKEN = "6227029117:AAH_hpycXzA1-QIXggh2COl99fcP1JAUJ70"
 
 
+dp = Dispatcher()
+filters.CommandStart
 
-bot = telebot.TeleBot('7079792810:AAE7N2lW5wRPOhLOWPE40sK72pUO2Rz3-d8')
-con = sqlite3.connect('bot_database.db')
-cursor = con.cursor()
-status = ''
-value = ''
-tg_id = None
-tg_name = None
-
-cursor.execute("""CREATE TABLE IF NOT EXISTS users
-               (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-               user_id INTEGER UNIQUE,
-               name TEXT NOT NULL,
-               mark INTEGER NOT NULL)
-            """)
-
-con.commit()
-cursor.close()
-con.close()
+@dp.message(filters.CommandStart())
+async def command_start_handler(message: types.Message):
+    await message.answer(f'Hello, {message.from_user.full_name}')
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    global tg_id, tg_name, status
-    tg_id = message.from_user.id
-    tg_name = message.from_user.username
-    if tg_name == 'Dovi_t':
-        status = 'admin'
-    else:
-        status = 'user'
-    bot.send_message(message.chat.id, "Hello")
-    markup = types.InlineKeyboardMarkup(row_width=3)
-    markup2 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    main_item = types.KeyboardButton("Ебашь!")
-    markup2.add(main_item)
-    item1 = types.InlineKeyboardButton("0", callback_data="0")
-    item2 = types.InlineKeyboardButton("1", callback_data="1")
-    item3 = types.InlineKeyboardButton("2", callback_data="2")
-    item4 = types.InlineKeyboardButton("3", callback_data="3")
-    item5 = types.InlineKeyboardButton("4", callback_data="4")
-    item6 = types.InlineKeyboardButton("5", callback_data="5")
-    markup.add(item1, item2, item3, item4, item5, item6)
-    if status == 'user':
-        bot.send_message(message.chat.id, "Оцените качество обслуживания:", reply_markup=markup)
-    else:
-        bot.send_message(message.chat.id, "Здравствуйте! Хотите посмотреть статистику?", reply_markup=markup2)
-        bot.register_next_step_handler(message, per)
+@dp.message()
+async def echo_handler(message: types.Message):
+    try:
+        await message.reply(message.text)
+    except TypeError:
+        await message.answer("Nice try!")
 
 
-def per(message):
-    if message.text == "Ебашь!":
-        con = sqlite3.connect("bot_database.db")
-        cursor = con.cursor()
-        cursor.execute("SELECT (name, mark) FROM users")
-        name, total = cursor.fetchall()
-        bot.send_message(message.chat.id, f'{name}, {total}')
-        con.commit()
-        cursor.close()
-        con.close()
+async def main():
+    bot = Bot(token=TOKEN)
+    await dp.start_polling(bot)
 
 
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-    global value, tg_id, tg_name
-    if call.data == "0":
-        value = call.data
-    elif call.data == "1":
-        value = call.data
-    elif call.data == "2":
-        value = call.data
-    elif call.data == "3":
-        value = call.data
-    elif call.data == "4":
-        value = call.data
-    elif call.data == "5":
-        value = call.data
-    con = sqlite3.connect('bot_database.db')
-    cursor = con.cursor()
-    cursor.execute("INSERT INTO users (user_id, name, mark) VALUES (?, ?, ?)", (tg_id, tg_name, value))
-    con.commit()
-    cursor.close()
-    con.close()
-    bot.send_message(call.message.chat.id, "Ваша оценка сохранена!")
-    
-
-
-bot.polling(none_stop=True)
-
+if __name__ == "__main__":
+    asyncio.run(main())
